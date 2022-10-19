9e14b1949da782d06b7f6455db7d50e0693d30ed
Carl Holzboog <caboog@gmail.com>
2022-10-19 15:22:18 -0700

    Main file

---
+++ csharp/azure/ad-adminunits/Program.cs
@@ -0,0 +1,31 @@
+﻿using Pulumi;
+using System.Collections.Generic;
+using AzureAD = Pulumi.AzureAD;
+
+
+return await Pulumi.Deployment.RunAsync(() =>
+
+{
+
+
+        var demoAdministrativeGroup = new AzureAD.Group("DemoAdministrativeGroup", new()
+    {
+        DisplayName = "DemoGroup",
+
+        SecurityEnabled = true,
+    });
+
+    var demoAdministrativeUnit = new AzureAD.AdministrativeUnit("demoAdministrativeUnit", new()
+    {
+        DisplayName = "Demo-AU",
+    });
+
+    var demoAdministrativeUnitMember = new AzureAD.AdministrativeUnitMember("demoAdministrativeUnitMember", new()
+    {
+        AdministrativeUnitObjectId = demoAdministrativeUnit.Id,
+        MemberObjectId = demoAdministrativeGroup.Id,
+    });
+
+
+});
+
