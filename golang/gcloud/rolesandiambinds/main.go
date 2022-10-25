e322f62aa367b7c767bea45ae91e08a05cb4b5c0
Carl Holzboog <caboog@gmail.com>
2022-10-25 15:58:14 -0700

    gcloud IAM demo work

---
+++ golang/gcloud/rolesandiambinds/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	serviceAccount "github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/serviceAccount"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/organizations"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		prom, err := serviceAccount.NewAccount(ctx, "prom-frontend", &serviceAccount.AccountArgs{
+			AccountId:   pulumi.String("prom-frontend"),
+			DisplayName: pulumi.String("Service Account"),
+		})
+		if err != nil {
+			return err
+		}
+		org, err := organizations.NewIAMBinding(ctx, "binding", &organizations.IAMBindingArgs{
+			Members: pulumi.StringArray{
+				pulumi.String("serviceaccount:prom-frontend@project-id.iam.gserviceaccount.com"),
+			},
+			OrgId: pulumi.String("1223456789"),
+			Role:  pulumi.String("roles/storage.admin"),
+		})
+		if err != nil {
+			return err
+		}
+		ctx.Export("org", org.ID())
+		ctx.Export("prom", prom.AccountId)
+		return nil
+	})
+}
